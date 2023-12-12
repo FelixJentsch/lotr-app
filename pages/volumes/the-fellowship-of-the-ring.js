2cf45a514cff7a74e1d34cf633bf0d4c33a84e54
FelixJentsch <FelixJentsch91@googlemail.com>
2023-12-12 13:55:00 +0100

    challenge solved except image display

---
+++ pages/volumes/the-fellowship-of-the-ring.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+import { volumes } from "@/resources/lib/data";
+
+const VolumeDetails = () => {
+  const volume = volumes.find(
+    (volume) => volume.slug === "the-fellowship-of-the-ring"
+  );
+
+  return (
+    <div>
+      <Link href="/volumes">← All Volumes</Link>
+      <h1>{volume.title}</h1>
+      <p>{volume.description}</p>
+      <ul>
+        {volume.books.map((book) => (
+          <li key={book.ordinal}>
+            {book.ordinal}. {book.title}
+          </li>
+        ))}
+      </ul>
+      <Image
+        src={`/public/images/${volume.cover}`}
+        width={140}
+        height={230}
+        alt={volume.title}
+      ></Image>
+    </div>
+  );
+};
+
+export default VolumeDetails;
